22a93ac7ebdf2d8c428eb1c4ea9f28280c6a3ee1
tanmayunicode <tanmay.unicodetechnologies@gmail.com>
2020-11-20 15:34:46 +0530

    new changes updated

---
+++ src/module1/LoginTest.java
@@ -0,0 +1,10 @@
+package module1;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Login Test");
+	}
+
+}
--- src/module1/RegisterTest.java
+++ src/module1/RegisterTest.java
@@ -5,6 +5,7 @@ public class RegisterTest {
 	public static void main(String[] args) {
 
 		System.out.println("This is Register Test");
+		System.out.println("Login test will be performed later");
 	}
 
 }
